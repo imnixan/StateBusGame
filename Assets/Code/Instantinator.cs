bb6d1c0a85fbc0f0f9696f386230321eaa663d5d
imnixan <vcmvenom@gmail.com>
2023-07-20 17:13:17 +0700

    init

---
+++ Assets/Code/Instantinator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+    public class Instantinator : MonoBehaviour
+    {
+
+    public static   RectTransform CreateRectTransform(string name, Vector2 position, Transform parent)
+    {
+        return Instantiate(new GameObject(name), position, new Quaternion(), parent).AddComponent<RectTransform>();
+    }
+        
+    public static Image CreateImage(string name, Vector2 position, Transform parent, Sprite sprite)
+    {
+        Image newImage = CreateRectTransform(name, position, parent).gameObject.AddComponent<Image>();
+        newImage.sprite = sprite;
+        return newImage;
+    }
+
+}
